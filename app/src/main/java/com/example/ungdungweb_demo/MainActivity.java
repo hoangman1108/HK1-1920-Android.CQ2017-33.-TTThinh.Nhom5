c64c608783269381efe0f2b7b13166d479bab469
hoangman188 <44186580+hoangman188@users.noreply.github.com>
2019-10-11 16:30:04 +0700

    create UI main

---
+++ app/src/main/java/com/example/ungdungweb_demo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ungdungweb_demo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
