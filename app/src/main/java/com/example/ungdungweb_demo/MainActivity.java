2cbabd4a6df757ee9532e80ecbd18a0cbc4103ad
camnhung-1411999 <55842532+camnhung-1411999@users.noreply.github.com>
2019-10-11 16:33:06 +0700

    Add UI Account

--- app/src/main/java/com/example/ungdungweb_demo/MainActivity.java
+++ app/src/main/java/com/example/ungdungweb_demo/MainActivity.java
@@ -14,10 +14,17 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout drawerLayout;
+    Button btnmain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 }
+
