adab700ee4df564e6715a4759d11ca02c724c8aa
hoangman188 <44186580+hoangman188@users.noreply.github.com>
2019-10-12 00:07:48 +0700

    man update

--- app/src/main/java/com/example/ungdungweb_demo/MainActivity.java
+++ app/src/main/java/com/example/ungdungweb_demo/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,8 +27,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //trang dể kết hiển thị các trang khác
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigationview);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment select;
+                    switch (menuItem.getItemId())
+                    {
+                        case R.id.itemHomeMain:
+                            select = new HomeFragment();
+                            break;
+                        case R.id.itemHomeMenu:
+                            select = new MenuMonFragment();
+                            break;
+                        default:
+                            throw new IllegalStateException("Unexpected value: " + menuItem.getItemId());
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, select).commit();
+
+                    return true;
+                }
+            };
 
 }
 
