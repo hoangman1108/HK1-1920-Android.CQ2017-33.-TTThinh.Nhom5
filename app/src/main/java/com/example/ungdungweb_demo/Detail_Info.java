07fbfe9fda3ef2fd14e862c3a7bd2e55c0016e68
camnhung-1411999 <55842532+camnhung-1411999@users.noreply.github.com>
2019-10-25 11:01:15 +0700

    Thêm tính năng sửa thông tin tài khoản

---
+++ app/src/main/java/com/example/ungdungweb_demo/Detail_Info.java
@@ -0,0 +1,116 @@
+package com.example.ungdungweb_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class Detail_Info extends AppCompatActivity {
+
+    Button btn_change;
+    TextView txtName,txtEmail,txtPhone,txtBirthday,txtGender;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail__info);
+
+        btn_change=findViewById(R.id.btn_change);
+        txtName=findViewById(R.id.detail_name);
+        txtEmail=findViewById(R.id.detail_email);
+        txtBirthday=findViewById(R.id.detail_birthday);
+        txtPhone=findViewById(R.id.detail_phone);
+        txtGender=findViewById(R.id.detail_gender);
+
+//        Intent intent=getIntent();
+//        Bundle bundle=intent.getBundleExtra("infor");
+//
+//        txtName.setText(bundle.getString("name"));
+//        txtPhone.setText(bundle.getString("phone"));
+//        txtEmail.setText(bundle.getString("email"));
+        btn_change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ChangeInformation();
+            }
+        });
+
+
+
+    }
+
+//    @Override
+//    public void onBackPressed() {
+//        super.onBackPressed();
+//
+//
+//        LayoutInflater factory = getLayoutInflater();
+//        @SuppressLint("ResourceType") View regisText = factory.inflate(R.layout.activity_main, null);
+////        TextView name1 = (TextView) regisText.findViewById(R.id.etail_name);
+//        NavigationView navigationView = regisText.findViewById(R.id.nav_view);
+//        Menu menu2 = navigationView.getMenu();
+//        menu2.findItem(R.id.itemName).setTitle(txtName.getText());
+////        String usr = name1.getText().toString();
+////        menu1.findItem(R.id.itemName).setTitle(usr);
+//        finish();
+//
+//    }
+
+    protected void ChangeInformation(){
+
+        final Dialog dialog=new Dialog(Detail_Info.this);
+        dialog.setContentView(R.layout.dialog_change_info);
+
+        Button dialog_btn_cancel=dialog.findViewById(R.id.dialog_btn_cancel);
+        Button dialog_btn_change=dialog.findViewById(R.id.dialog_btn_change);
+        final EditText dialog_detail_name=dialog.findViewById(R.id.dialog_detail_name);
+        final EditText dialog_detail_email=dialog.findViewById(R.id.dialog_detail_email);
+        final EditText dialog_detail_birthday=dialog.findViewById(R.id.dialog_detail_birthday);
+        final EditText dialog_detail_phone=dialog.findViewById(R.id.dialog_detail_phone);
+        final RadioButton radioButton_male=dialog.findViewById(R.id.dialog_rabtn_male);
+        final RadioButton radioButton_female=dialog.findViewById(R.id.dialog_rabtn_female);
+        dialog_detail_name.setText(txtName.getText());
+        dialog_detail_email.setText(txtEmail.getText());
+        dialog_detail_birthday.setText(txtBirthday.getText());
+        dialog_detail_phone.setText(txtPhone.getText());
+        dialog_btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog_btn_change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                txtName.setText(dialog_detail_name.getText());
+                txtBirthday.setText(dialog_detail_birthday.getText());
+                txtEmail.setText(dialog_detail_email.getText());
+                txtPhone.setText(dialog_detail_phone.getText());
+                if(radioButton_female.isChecked())
+                {
+                    txtGender.setText(radioButton_female.getText());
+                }
+                else
+                {
+                    txtGender.setText(radioButton_male.getText());
+                }
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+
+    }
+}
+
--- app/src/main/java/com/example/ungdungweb_demo/MainActivity.java
+++ app/src/main/java/com/example/ungdungweb_demo/MainActivity.java
@@ -10,19 +10,28 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private DrawerLayout drawerLayout;
     Button btnmain;
     Intent intent;
+    NavigationView navigationView;
+    Menu menu1;
 
     ImageButton imgbtnmaps;
     @Override
@@ -34,10 +43,28 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigationview);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
 
+        navigationView=findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 //        FragmentManager fragmentManager = getSupportFragmentManager();
 //        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
        // imgbtnmaps = (ImageButton) findViewById(R.id.btnDiaChi);
+
+//        Intent intent1 = getIntent();
+//        Bundle bundle1 = intent1.getBundleExtra("info1");
+//       if(!bundle1.isEmpty()){
+//           String name1 = bundle1.getString("name");
+//           menu1 = navigationView.getMenu();
+////        menu1.findItem(R.id.itemName).setTitle(bundle1.getString("name"));
+//           menu1.findItem(R.id.itemName).setTitle(name1);
+//       }
+
+        Menu menu1 = navigationView.getMenu();
+        LayoutInflater factory = getLayoutInflater();
+        View regisText = factory.inflate(R.layout.activity_detail__info, null);
+        TextView name1 = (TextView) regisText.findViewById(R.id.detail_name);
+        String usr = name1.getText().toString();
+        menu1.findItem(R.id.itemName).setTitle(usr);
 
         Fragment fragmentfirst = new HomeFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragmentfirst).show(fragmentfirst).commit();
@@ -73,6 +100,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void Show() {
         HomeFragment homeFragment = (HomeFragment) this.getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.itemInfor:
+//                Menu menu=navigationView.getMenu();
+//                String name=(String)menu.findItem(R.id.itemName).getTitle();
+//                String phone=(String)menu.findItem(R.id.itemSdt).getTitle();
+//                String email=(String)menu.findItem(R.id.itemEmail).getTitle();
+                Intent intent=new Intent(this, Detail_Info.class);
+//                Bundle bundle=new Bundle();
+//                bundle.putString("name",name);
+//                bundle.putString("phone",phone);
+//                bundle.putString("email",email);
+//                intent.putExtra("infor",bundle);
+                startActivity(intent);
+
+
+                break;
+            case R.id.itemHelp:
+                default:
+                    Toast.makeText(this, "Not found", Toast.LENGTH_SHORT).show();
+
+        }
+
+        return true;
     }
 }
 
