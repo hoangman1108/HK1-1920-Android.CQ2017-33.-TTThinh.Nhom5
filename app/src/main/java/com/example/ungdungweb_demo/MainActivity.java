d6a4c17a9989960df35c65eeec6941f89dbb9a85
phatfood <baophatnguyen99@gmail.com>
2019-11-17 04:36:35 +0700

    Splash

--- app/src/main/java/com/example/ungdungweb_demo/MainActivity.java
+++ app/src/main/java/com/example/ungdungweb_demo/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
